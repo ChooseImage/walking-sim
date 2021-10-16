2d2d4522e645ac43d71be13f738f64b795c03228
ChooseImage <chooseimage0125.com>
2021-10-16 13:28:42 -0400

    landscaped

--- walking-sim/Assets/LT.cs
+++ walking-sim/Assets/LT.cs
@@ -14,6 +14,33 @@ public class LT : MonoBehaviour
     public GameObject Block7;
     public GameObject Block8;
     public GameObject Block9;
+    public GameObject Block10;
+    public GameObject Block11;
+    public GameObject Block12;
+    public GameObject Block13;
+    public GameObject Block14;
+    public GameObject Block15;
+    public GameObject Block16;
+    public GameObject Block17;
+    public GameObject Block18;
+    public GameObject Block19;
+    public GameObject Block20;
+    public GameObject Block21;
+    public GameObject Block22;
+    public GameObject Block23;
+    public GameObject Block24;
+    public GameObject Block25;
+    public GameObject Block26;
+    public GameObject Block27;
+    public GameObject Block28;
+    public GameObject Block29;
+    public GameObject Block30;
+    public GameObject Block31;
+    public GameObject Block32;
+    public GameObject Block33;
+    public GameObject Block34;
+    public GameObject Block35;
+    public GameObject Block36;
    //public GameObject Block2;
 
 
@@ -29,6 +56,33 @@ public class LT : MonoBehaviour
          LeanTween.moveY(Block7, 5, 13).setLoopPingPong();
          LeanTween.moveY(Block8, 7, 9).setLoopPingPong();
          LeanTween.moveY(Block9, 9, 13).setLoopPingPong();
+         LeanTween.moveY(Block10, 3, 3).setLoopPingPong();
+         LeanTween.moveY(Block11, 7, 14).setLoopPingPong();
+         LeanTween.moveY(Block12, 7, 8).setLoopPingPong();
+         LeanTween.moveY(Block13, 7, 7).setLoopPingPong();
+         LeanTween.moveY(Block14, 9, 4).setLoopPingPong();
+         LeanTween.moveY(Block15, 8, 9).setLoopPingPong();
+         LeanTween.moveY(Block16, 5, 13).setLoopPingPong();
+         LeanTween.moveY(Block17, 7, 9).setLoopPingPong();
+         LeanTween.moveY(Block18, 9, 13).setLoopPingPong();
+         LeanTween.moveY(Block19, 3, 3).setLoopPingPong();
+         LeanTween.moveY(Block20, 7, 14).setLoopPingPong();
+         LeanTween.moveY(Block21, 7, 8).setLoopPingPong();
+         LeanTween.moveY(Block22, 7, 7).setLoopPingPong();
+         LeanTween.moveY(Block23, 9, 4).setLoopPingPong();
+         LeanTween.moveY(Block24, 8, 9).setLoopPingPong();
+         LeanTween.moveY(Block25, 5, 13).setLoopPingPong();
+         LeanTween.moveY(Block26, 7, 9).setLoopPingPong();
+         LeanTween.moveY(Block27, 9, 13).setLoopPingPong();
+         LeanTween.moveY(Block28, 3, 3).setLoopPingPong();
+         LeanTween.moveY(Block29, 7, 14).setLoopPingPong();
+         LeanTween.moveY(Block30, 7, 8).setLoopPingPong();
+         LeanTween.moveY(Block31, 7, 7).setLoopPingPong();
+         LeanTween.moveY(Block32, 9, 4).setLoopPingPong();
+         LeanTween.moveY(Block33, 8, 9).setLoopPingPong();
+         LeanTween.moveY(Block34, 5, 13).setLoopPingPong();
+         LeanTween.moveY(Block35, 7, 9).setLoopPingPong();
+         LeanTween.moveY(Block36, 9, 13).setLoopPingPong();
     }
 
     // Update is called once per frame
